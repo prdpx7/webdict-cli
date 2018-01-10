b561546e2c6d6a525af3af01983193fb99944f65
zuck007 <pradeepchoudhary009@gmail.com>
2018-01-10 18:10:20 +0530

    corrected typo

--- test.js
+++ test.js
@@ -5,8 +5,3 @@ test('webdict --help', async t => {
     // console.log(help_stdout);
   t.true(helpStdout.stdout.length > 0)
 })
-test('searching `awordwhichdoesnotexist` in urbandictionary', async t => {
-  const ansStdout = await execa('./webdict.js', ['-u', 'awordwhichdoesnotexist'])
-  // console.log(ansStdout)
-  t.is(ansStdout.stdout, 'check the spelling again')
-})
--- webdict.js
+++ webdict.js
@@ -5,17 +5,19 @@ const program = require('commander')
 const chalk = require('chalk')
 const pkgversion = require('./package.json').version
 
-function fetchDefiniton (site, word) {
+function fetchDefinition (site, word) {
   webdict(site, word)
         .then(resp => {
           if (resp.message === 'success') {
             console.log(chalk.bold('Type: ') + chalk.green(resp.type))
             console.log(chalk.bold('Source: ') + chalk.green(resp.source))
             resp.definition.forEach((val, idx) => {
-              console.log(chalk.bold('Defintion', idx + 1) + ':\n' + chalk.green(val))
+              console.log(chalk.bold('Definition', idx + 1) + ':\n' + chalk.green(val))
             })
           } else {
-            console.log(chalk.red(`${resp.message}`))
+            console.log(chalk.bold('-----------------'))
+            console.log(chalk.bold('From: ') + chalk.green(resp.source))
+            console.log(chalk.bold('Message: ') + chalk.red(`${resp.message}`))
           }
         })
 }
@@ -26,10 +28,15 @@ program
     .option('-d , --dictionary <word>', 'search in dictionary.com')
     .option('-u, --urbandictionary <word>', 'search in urbandictionary.com')
     .parse(process.argv)
+
 if (program.dictionary) {
-  fetchDefiniton('dictionary', program.dictionary)
+  fetchDefinition('dictionary', program.dictionary)
 } else if (program.urbandictionary) {
-  fetchDefiniton('urbandictionary', program.urbandictionary)
+  fetchDefinition('urbandictionary', program.urbandictionary)
+} else if (program.args.length > 0) {
+  var word = program.args.join(' ')
+  fetchDefinition('dictionary', word)
+  fetchDefinition('urbandictionary', word)
 } else {
   console.log('see `webdict --help` for more info')
 }
